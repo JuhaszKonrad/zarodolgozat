fd6dee8d751a6c331cca64ce11eeba64dd7868b2
JuhaszKonrad <113589949+JuhaszKonrad@users.noreply.github.com>
2023-04-20 19:34:50 +0200

    Add files via upload

---
+++ partials-frontend/footer.php
@@ -0,0 +1,29 @@
+<!-- Social Media  -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/ios-filled/50/null/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/ios-filled/50/null/instagram-new--v1.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/ios/50/null/twitter--v1.png"/></a>
+                </li>
+            </ul>
+
+        </div>
+    </section>
+
+    <!-- Footer  -->
+    <section class="footer">
+        <div class="container">
+            <p class="text-center">Tervezte <a href="#">Juhász Konrád</a>. Készült 2022-2023.</p>
+            
+        </div>
+    </section>
+
+</body>
+
+</html>
---
+++ partials-frontend/menu.php
@@ -0,0 +1,45 @@
+<?php include('config/konstansok.php');?>
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="képek/favicon-32x32.png">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Étel rendelés</title>
+</head>
+
+<body>
+
+    <!-- Navbar  -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="<?php echo HOME_URL;?>"><img src="képek/logo.png" alt="Logo" class="img-responsive"></a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="<?php echo HOME_URL;?>">Főoldal</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo HOME_URL;?>categories.php">Kategóriák</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo HOME_URL;?>foods.php">Ételek</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo HOME_URL;?>contact.php">Kapcsolat</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="clearfix">
+
+            </div>
+
+        </div>
+    </section>
